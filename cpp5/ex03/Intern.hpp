736c35dd51655d1185aa7176034d31738f18ed28
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-01-23 23:24:09 +0100

    psuh of the day

--- cpp5/ex03/Intern.hpp
+++ cpp5/ex03/Intern.hpp
@@ -6,15 +6,49 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 18:53:32 by simon             #+#    #+#             */
-/*   Updated: 2025/01/22 19:24:38 by simon            ###   ########.fr       */
+/*   Updated: 2025/01/23 22:59:50 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+# ifndef INTERN_HPP
+# define INTERN_HPP
+# include "AForm.hpp"
+# include "PresidentialPardonForm.hpp"
+# include "RobotomyRequestForm.hpp"
+# include "ShrubberyCreationForm.hpp"
+# include <string.h>
+
+class	Intern
+{
+	private:
+
+	public:
+		Intern();
+		~Intern();
+		Intern(Intern const &copy);
+		Intern const&	operator=(Intern const& copy);
+		
+		AForm*	makeForm(std::string const form_name, std::string const target_form);
+		
+		class IncorrectFormName: public std::exception
+		{
+			public:
+				virtual const char *what(void) const throw();
+		};
+};
+
+#endif*/
 
 #ifndef INTERN_HPP
 #define	INTERN_HPP
 
 #include <iostream>
 #include "Bureaucrat.hpp"
+# include "PresidentialPardonForm.hpp"
+# include "RobotomyRequestForm.hpp"
+# include "ShrubberyCreationForm.hpp"
+# include <string.h>
 
 class	Intern
 {
--- cpp5/ex03/main.cpp
+++ cpp5/ex03/main.cpp
@@ -6,27 +6,60 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 18:46:59 by simon             #+#    #+#             */
-/*   Updated: 2025/01/22 19:57:35 by simon            ###   ########.fr       */
+/*   Updated: 2025/01/23 22:57:58 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
-#include "PresidentialPardonForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "ShrubberyCreationForm.hpp"
 #include "Intern.hpp"
+
+// int	main(void)
+// {
+// 	{
+// 		Bureaucrat	Bender("Bender");
+// 		Bender.setGrade(3);
+// 		PresidentialPardonForm	Pact("Pact");
+
+// 		Intern someRandomIntern;
+// 		AForm* rrf;
+// 		rrf = someRandomIntern.makeForm("Presidential", "Bender");
+// 		delete rrf;
+// 	}
+// 	return (0);
+// }
 
 int	main(void)
 {
-	{
-		Bureaucrat	Bender("Bender");
-		Bender.setGrade(3);
-		PresidentialPardonForm	Pact("Pact");
+	AForm *form1;
 
-		Intern someRandomIntern;
-		AForm* rrf;
-		rrf = someRandomIntern.makeForm("Presidential", "Bender");
-		delete rrf;
+	try
+	{
+		std::cout << "_______TEST 1_______" << std::endl;
+		Bureaucrat	test1("test1");
+		test1.setGrade(150);
+		// Bureaucrat	// test2("// test2", 1);
+
+		std::cout << test1 << std::endl;
+		// std::cout << // test2 << std::endl;
+
+		Intern	someRandomIntern;
+
+		form1 = someRandomIntern.makeForm("presidential pardin", "exampleForm1");
+		std::cout << *form1 << std::endl;
+
+		// test2.signForm(*form1);
+
+		std::cout << *form1 << std::endl;
+
+		// test2.executeForm(*form1);
+		test1.executeForm(*form1);
+
+		delete form1;
 	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
 	return (0);
 }
