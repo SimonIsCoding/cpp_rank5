e31c397f1dd347ffff8ec48b8fdd862a26cd2371
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-02-02 12:26:31 +0100

    cpp06 done

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 19:02:48 by simon             #+#    #+#             */
-/*   Updated: 2025/02/01 20:41:38 by simon            ###   ########.fr       */
+/*   Updated: 2025/02/02 12:25:02 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int	main(void)
 	Base*	example;
 
 	example = generate();
-	// std::cout << "example memory: " << example << std::endl;
 	identify(example);
+	identify(*example);
 	delete example;
 	return (0);
 }
