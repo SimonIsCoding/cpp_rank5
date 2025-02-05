d43cd2f616d4ffb41941796456f59ae270d72a8c
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-02-05 15:59:22 +0100

    done with cpp08 ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,20 +6,65 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 19:35:00 by simon             #+#    #+#             */
-/*   Updated: 2025/02/04 19:35:38 by simon            ###   ########.fr       */
+/*   Updated: 2025/02/05 15:58:25 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	EASYFIND_HPP
 #define EASYFIND_HPP
 
-#include <iostream>
+# include <iostream>
+# include <vector>
+# include <exception>
+# include <algorithm>
 
-class	easyfind
+// template <typename T, int I>
+// class	easyfind
+// {
+// 	private:
+
+// 	public:
+// 		easyfind();
+// 		easyfind(easyfind const& copy);
+// 		easyfind const& operator=(easyfind const& copy);
+// 		~easyfind();
+// };
+
+// template <typename T, int I>
+// easyfind::easyfind()
+// {
+// 	std::cout << "Default Constructor Called" << std::endl;
+// }
+
+class	NumberNotFound:	public std::exception
 {
-	private:
-
 	public:
+		const char *what() const throw()
+		{
+			return ("Number not found");
+		}
 };
+
+template <typename T>
+int	find_in_containers(T container, int to_find)
+{
+	typename	T::iterator	it;
+	int			index = 0;
+	try {
+	{
+		for (it = container.begin(); it != container.end(); it++, index++)
+		{
+			if (*it == to_find)
+				return (index);
+		}
+		throw NumberNotFound();
+	}
+	}
+	catch(const NumberNotFound &e)
+	{
+		std::cerr << "Error when finding " << to_find << ". " << e.what() << std::endl;
+	}
+	return (-1);
+}
 
 #endif
