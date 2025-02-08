68c487b082c6875f047f693301375ed3b5c4cf2e
SimArch21 <simon.archambault@hotmail.fr>
2025-02-08 17:50:19 +0100

    working on cpp08 ex01

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 16:05:49 by simon             #+#    #+#             */
-/*   Updated: 2025/02/08 17:24:22 by simarcha         ###   ########.fr       */
+/*   Updated: 2025/02/08 17:49:57 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,19 @@
 
 int	main(void)
 {
-	/*{
-		Span sp = Span(5);
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
-	}*/
 	{
 		Span	test(5);
 		test.addNumber();
 		test.addNumber();
-		// test.addNumber();
-		// test.addNumber();
-		// test.addNumber();
-		// test.addNumber();
+		test.addNumber();
+		test.addNumber();
+		test.addNumber();
+		test.addNumber();
 		test.displayAllNumbers();
 		try
 		{
-			std::cout << "longest span " << test.longestSpan() << std::endl;
+			std::cout << "longest span : " << test.longestSpan() << std::endl;
+			std::cout << "shortest span: " << test.shortestSpan() << std::endl;
 		}
 		catch (const std::exception& e)
 		{
