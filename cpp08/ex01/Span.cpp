c007a28d9f9d2026e9cf7e5d5d95b0a17a031891
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-02-08 15:49:18 +0100

    working on cpp08 ex01

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Span.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
+/*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 14:51:51 by simon             #+#    #+#             */
-/*   Updated: 2025/02/06 15:05:21 by simon            ###   ########.fr       */
+/*   Updated: 2025/02/08 15:48:36 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 Span::Span(){}
 
-Span::~Span(){}
+Span::~Span()
+{
+}
 
 Span::Span(unsigned int N) : _N(N)
-{}
+{
+}
 
 Span::Span(Span const& copy)
 {
@@ -36,11 +39,42 @@ Span const& Span::operator=(Span const& copy)
 	return (*this);
 }
 
-		void	addNumber();
-		void	shortestSpan();
-		void	longestSpan();
+const char *Span::TooManyNumbers::what() const throw()
+{
+	return ("We can't add any numbers. We reached the maximum possible numbers stored");
+}
+
+void	Span::displayAllNumbers() const
+{
+	for (unsigned int i = 0; i < _vec.size(); i++)
+		std::cout << _vec[i] << std::endl;
+}
 
 void	Span::addNumber()
+{
+	int	nb;
+
+	srand(static_cast<unsigned int>(time(NULL)));
+  	nb = rand();
+	try
+	{
+		if (_vec.size() < _N)
+			_vec.push_back(nb);
+		else
+			throw (Span::TooManyNumbers());
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
+
+void	Span::shortestSpan()
+{
+	
+}
+
+void	Span::longestSpan()
 {
 	
 }
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
+/*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 16:15:03 by simon             #+#    #+#             */
-/*   Updated: 2025/02/06 14:51:37 by simon            ###   ########.fr       */
+/*   Updated: 2025/02/08 15:44:24 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,29 @@
 #define	SPAN_HPP
 
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime> 
 
 class Span
 {
 	private:
-		unsigned int	_N;
+		unsigned int		_N;
+		std::vector<int>	_vec;
 	public:
 		Span();
 		Span(unsigned int N);
 		Span(Span const& copy);
 		Span const& operator=(Span const& copy);
 		~Span();
+
+		class TooManyNumbers: public std::exception
+		{
+			public:
+				virtual const char *what(void) const throw();
+		};
+
+		void	displayAllNumbers() const;
 
 		void	addNumber();
 		void	shortestSpan();
