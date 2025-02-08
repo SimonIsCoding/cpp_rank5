f9b4120e1bfb5272b620001ab5836fc1586ad0ac
SimArch21 <simon.archambault@hotmail.fr>
2025-02-08 19:10:49 +0100

    cpp08 setting ex02

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 19:01:14 by simarcha          #+#    #+#             */
+/*   Updated: 2025/02/08 19:06:28 by simarcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <stack>
+
+class MutantStack
+{
+	public:
+
+	private:
+			
+};
+
+#endif
---
+++ cpp08/ex02/MutantStack.tpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.tpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 19:07:23 by simarcha          #+#    #+#             */
+/*   Updated: 2025/02/08 19:10:27 by simarcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_TPP
+#define MUTANTSTACK_TPP
+
+template <typename T>
+void	name_function()
+{
+	
+}
+
+#endif
