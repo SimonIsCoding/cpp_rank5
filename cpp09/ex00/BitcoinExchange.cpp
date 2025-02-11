cfc9c4f30a6b89e6bff24a8b2351bde35b5f0a74
SimArch21 <simon.archambault@hotmail.fr>
2025-02-11 19:40:03 +0100

    working on cpp09 ex00

---
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 18:17:18 by simarcha          #+#    #+#             */
+/*   Updated: 2025/02/11 18:26:52 by simarcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+// check si on peut ouvrir le fichier => ou si le fichier existe
+// Each line in this file must use the following format: "date | value".
+// A valid date will always be in the following format: Year-Month-Day.
+// A valid value must be either a float or a positive integer, between 0 and 1000.
---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 18:15:21 by simarcha          #+#    #+#             */
+/*   Updated: 2025/02/11 19:30:00 by simarcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <cctype>
+
+class	BitcoinExchange
+{
+	private:
+		
+	public:
+
+};
+
+#endif
