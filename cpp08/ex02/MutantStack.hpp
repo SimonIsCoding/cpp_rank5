7766444454172069b133b658ca54504edbb7e45f
SimArch21 <simon.archambault@hotmail.fr>
2025-02-11 16:59:09 +0100

    cpp08 done

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 19:01:14 by simarcha          #+#    #+#             */
-/*   Updated: 2025/02/08 19:06:28 by simarcha         ###   ########.fr       */
+/*   Updated: 2025/02/11 16:27:32 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,38 @@
 #include <algorithm>
 #include <stack>
 
-class MutantStack
+template <typename T>
+class MutantStack : public std::stack<T>
 {
 	public:
+		MutantStack() : std::stack<T>() {}
+		
+		~MutantStack(){}
 
-	private:
-			
+		MutantStack(const std::stack<T> &ref) : std::stack<T>()
+		{
+			std::cout << "Mutant Stack copy constructor called\n";
+			(void)ref;
+		}
+		
+		std::stack<T> &operator=(const std::stack<T> &ref)
+		{
+			std::cout << "Mutant Stack assignation operator called\n";
+			if (this != &ref)
+				this = &ref;
+			return (*this);
+		}
+		
+		typedef typename std::deque<T>::iterator	iterator;
+		iterator	begin()
+		{
+			return (this->c.begin());
+		}
+
+		iterator	end()
+		{
+			return (this->c.end());
+		}
 };
 
 #endif
