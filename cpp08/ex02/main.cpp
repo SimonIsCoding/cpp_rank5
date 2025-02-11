cc051897d8fd7402683a3717d4ad2fc1241f8125
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-02-11 16:59:09 +0100

    cpp08 done

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,20 +6,22 @@
 /*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 18:53:01 by simarcha          #+#    #+#             */
-/*   Updated: 2025/02/08 19:04:07 by simarcha         ###   ########.fr       */
+/*   Updated: 2025/02/11 16:58:14 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
 
+//always remember that in a stack, we add elements such as the last in will be the first out
 int	main(void)
 {
 	{
 		MutantStack<int> mstack;
 		mstack.push(5);
 		mstack.push(17);
-		std::cout << mstack.top() << std::endl;
-		mstack.pop();
+		std::cout << mstack.top() << std::endl;//returns the topmost element of the stack
+		mstack.pop();//remove the upper element
 		std::cout << mstack.size() << std::endl;
 		mstack.push(3);
 		mstack.push(5);
@@ -37,4 +39,29 @@ int	main(void)
 		}
 		std::stack<int> s(mstack);
 	}
+	{
+		std::cout << "----------------other test with list----------------\n";
+		std::list<int> mstack;
+		mstack.push_back(5);
+		mstack.push_back(17);
+		std::cout << mstack.back() << std::endl;
+		mstack.pop_back();
+		std::cout << mstack.size() << std::endl;
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		//[...]
+		mstack.push_back(0);
+		std::list<int>::iterator it = mstack.begin();
+		std::list<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::list<int> s(mstack);
+	}
+	return (0);
 }
