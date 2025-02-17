c81b4b2bca66ff6f3d5b7e1de3856bfa426574be
SimArch21 <simon.archambault@hotmail.fr>
2025-02-17 18:04:07 +0100

    working ex01 cpp09

--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 14:15:44 by simon             #+#    #+#             */
-/*   Updated: 2025/02/17 15:04:09 by simon            ###   ########.fr       */
+/*   Updated: 2025/02/17 17:24:18 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 #include <iostream>
 #include <stack>
 #include <algorithm>
+#include <sstream>
 
 #endif
--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 14:14:46 by simon             #+#    #+#             */
-/*   Updated: 2025/02/17 16:59:34 by simon            ###   ########.fr       */
+/*   Updated: 2025/02/17 18:03:09 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,46 @@
 int	is_sign(char c)
 {
 	char arr[4] = {'+', '-', '*', '/'};
-	return std::find(arr, arr + 4, c) != (arr + 4);
+    for (int i = 0; i < 4; i++)
+        if (arr[i] == c)
+            return i;
+    return -1;
 }
 
 void print_stack(std::stack<std::string> stack)
 {
+	std::cout << "STACK__________________________\n";
 	while (!stack.empty())
 	{
 		std::cout << stack.top() << std::endl;
 		stack.pop();
 	}
+	std::cout << "_______________________________\n";
 }
 
 std::stack<std::string>	create_stack(char *argv_1)
 {
 	std::string	input = argv_1;
-	int		i = 0;
+	int			i = 0;
 	std::stack<std::string> stack;
-	int		flag;
-	// long	result;
+	int			flag;
+	long		result = 0;
 	
-	// result = 0;
 	while (input[i] != '\0')
 	{
 		while (input[i] == ' ')
 			i++;
+		std::cout << "1 && i = _" << i << "_ && input[i] = _" << input[i] << "_\n";
 		if (is_sign(input[i]) != -1 && !std::isdigit(input[i + 1]))
 		{
-			std::string str(1, input[i]);
-			stack.push(str);
+			std::cout << "2\n";
+			// std::string str(1, input[i]);
+			// stack.push(str);
 			flag = 0;
 		}
 		else if (std::isdigit(input[i]) || (input[i] == '-' && std::isdigit(input[i + 1])))
 		{
+			std::cout << "3\n";
 			int j = i;
 			if (input[i] == '-')
 				j++;
@@ -56,15 +63,48 @@ std::stack<std::string>	create_stack(char *argv_1)
 			stack.push(input.substr(i, j - i));
 			i += j - i - 1;
 			flag = 1;
+			print_stack(stack);
 		}
 		if (flag == 0)
 		{
-			
+			std::cout << "4\n";
+			if (stack.size() <= 1)
+			{
+				std::cerr << "Error 4\n";
+				exit(1);
+			}
+			int f1 = atoi(stack.top().c_str());
+			stack.pop();
+			int f2 = atoi(stack.top().c_str());
+			stack.pop();
+			int k = is_sign(input[i]);
+			std::cout << "k = " << k << std::endl;
+			std::cout << "f1 = " << f1 << std::endl;
+			std::cout << "f2 = " << f2 << std::endl;
+			switch(k)
+			{
+				case 0:
+					result = f1 + f2;
+					break ;
+				case 1:
+					result = f1 - f2;
+					break ;
+				case 2:
+					result = f1 * f2;
+					break ;
+				case 3:
+					result = f1 / f2;
+					break ;
+			}
+			std::stringstream ss;
+			ss << result;
+			std::string result_str = ss.str();
+			stack.push(result_str);
 		}
 		i++;
 	}
 	print_stack(stack);
-	(void)flag;
+	std::cout << "result: " << result << std::endl;
 	return (stack);
 }
 
